a779c66042fca9975881156e7c29c46472692759
Ayutac <fly.high.android@gmail.com>
2025-10-14 02:03:59 +0200

    Surviving ignition now bestows flame.

--- common/src/main/java/studio/abos/mc/sunspot/common/component/entity/CommonFlamefallFireComponent.java
+++ common/src/main/java/studio/abos/mc/sunspot/common/component/entity/CommonFlamefallFireComponent.java
@@ -2,10 +2,12 @@ package studio.abos.mc.sunspot.common.component.entity;
 
 import net.minecraft.core.Registry;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageType;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
 import org.jetbrains.annotations.NotNull;
 import studio.abos.mc.sunspot.common.registry.SPDamageTypeRegistry;
 import studio.abos.mc.sunspot.platform.SPComponentPlatformUtils;
@@ -45,13 +47,28 @@ public abstract class CommonFlamefallFireComponent {
 
     public static void serverTick(final @NotNull Entity entity, final @NotNull Registry<DamageType> damageTypes) {
         final CommonFlamefallFireComponent flamefallFire = SPComponentPlatformUtils.getFlamefallFireData(entity);
-        if (flamefallFire == null) {
+        if (flamefallFire == null || flamefallFire.getRemainingFireTicks() == 0) {
             return;
         }
-        if (entity instanceof final LivingEntity living && living.isAlive() && flamefallFire.getRemainingFireTicks() % TICKS_FOR_DAMAGE == 1) {
+        if (entity instanceof final LivingEntity living && living.isAlive() && flamefallFire.getRemainingFireTicks() % TICKS_FOR_DAMAGE == 0) {
             living.hurt(new DamageSource(damageTypes.getHolderOrThrow(SPDamageTypeRegistry.FLAMEFALL_FIRE)), DAMAGE);
         }
         flamefallFire.decreaseRemainingFireTicks();
+        // bestow flame
+        if (flamefallFire.getRemainingFireTicks() == 0 && entity instanceof final LivingEntity living && living.isAlive()) {
+            if (living instanceof final Mob mob) {
+                final CommonFlameComponent flame = SPComponentPlatformUtils.getFlameData(mob);
+                if (flame != null && !flame.isFlametouched()) {
+                    flame.setFlametouched(true);
+                }
+            }
+            else if (living instanceof final ServerPlayer player) {
+                final CommonFlameComponent flame = SPComponentPlatformUtils.getFlameData(player);
+                if (flame != null && !flame.isFlametouched()) {
+                    flame.setFlametouched(true, player);
+                }
+            }
+        }
     }
 
     public void writeNbt(final CompoundTag nbt) {
