7baac1f21b345cb235887aba7eff901708dff745
Ayutac <fly.high.android@gmail.com>
2025-10-17 20:27:47 +0200

    Abstracted {Revitalise} BE.

---
+++ common/src/main/java/studio/abos/mc/sunspot/common/blockentity/EffectGlyphBlockEntity.java
@@ -0,0 +1,42 @@
+package studio.abos.mc.sunspot.common.blockentity;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
+import org.jetbrains.annotations.NotNull;
+import studio.abos.mc.sunspot.common.registry.SPTagRegistry;
+
+import java.util.List;
+
+public abstract class EffectGlyphBlockEntity extends GlyphBlockEntity {
+
+    protected EffectGlyphBlockEntity(final @NotNull BlockEntityType<? extends EffectGlyphBlockEntity> blockEntityType, final @NotNull BlockPos blockPos, final @NotNull BlockState blockState) {
+        super(blockEntityType, blockPos, blockState);
+    }
+
+    public abstract int getInterval();
+
+    public abstract int getRadius();
+
+    public abstract Holder<MobEffect> getEffect();
+
+    public static void tick(final @NotNull Level level, final @NotNull BlockPos pos, final @NotNull BlockState state, final @NotNull EffectGlyphBlockEntity blockEntity) {
+        GlyphBlockEntity.tick(level, pos, state, blockEntity);
+        if (level.getGameTime() % blockEntity.getInterval() == 0) {
+            final List<LivingEntity> entities = level.getEntitiesOfClass(LivingEntity.class, new AABB(pos).inflate(blockEntity.getRadius()), entity -> !(entity instanceof final Player player && player.isSpectator()));
+            for (final LivingEntity entity : entities) {
+                if (!entity.getType().is(SPTagRegistry.UNAFFECTED_BY_REVITALISE)) {
+                    entity.addEffect(new MobEffectInstance(blockEntity.getEffect(), blockEntity.getInterval()));
+                }
+            }
+        }
+    }
+
+}
--- common/src/main/java/studio/abos/mc/sunspot/common/blockentity/RevitaliseBlockEntity.java
+++ common/src/main/java/studio/abos/mc/sunspot/common/blockentity/RevitaliseBlockEntity.java
@@ -1,13 +1,16 @@
 package studio.abos.mc.sunspot.common.blockentity;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
 import studio.abos.mc.sunspot.common.registry.SPBlockEntityTypeRegistry;
 
-public class RevitaliseBlockEntity extends GlyphBlockEntity {
+public class RevitaliseBlockEntity extends EffectGlyphBlockEntity {
 
     public RevitaliseBlockEntity(final BlockPos blockPos, final BlockState blockState) {
         this(SPBlockEntityTypeRegistry.REVITALISE.get(), blockPos, blockState);
@@ -16,11 +19,25 @@ public class RevitaliseBlockEntity extends GlyphBlockEntity {
 
     protected RevitaliseBlockEntity(final @NotNull BlockEntityType<? extends RevitaliseBlockEntity> blockEntityType, final @NotNull BlockPos blockPos, final @NotNull BlockState blockState) {
         super(blockEntityType, blockPos, blockState);
-        setMaxFlame(100);
+    }
+
+    @Override
+    public int getInterval() {
+        return 80;
+    }
+
+    @Override
+    public int getRadius() {
+        return 5;
+    }
+
+    @Override
+    public Holder<MobEffect> getEffect() {
+        return MobEffects.REGENERATION;
     }
 
     public static void tick(final @NotNull Level level, final @NotNull BlockPos pos, final @NotNull BlockState state, final @NotNull RevitaliseBlockEntity blockEntity) {
-        GlyphBlockEntity.tick(level, pos, state, blockEntity);
+        EffectGlyphBlockEntity.tick(level, pos, state, blockEntity);
     }
 
 }
