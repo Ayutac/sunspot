3837aaac32612ded2edaafaf23907a7eeb3748ba
Ayutac <fly.high.android@gmail.com>
2025-10-17 15:10:53 +0200

    Made {OFFSET} actually use energy.

--- common/src/main/java/studio/abos/mc/sunspot/common/blockentity/OffsetBlockEntity.java
+++ common/src/main/java/studio/abos/mc/sunspot/common/blockentity/OffsetBlockEntity.java
@@ -3,6 +3,7 @@ package studio.abos.mc.sunspot.common.blockentity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Vec3i;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -10,6 +11,7 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
+import studio.abos.mc.sunspot.common.block.LatticeManifestBlock;
 import studio.abos.mc.sunspot.common.registry.SPBlockEntityTypeRegistry;
 import studio.abos.mc.sunspot.common.registry.SPBlockRegistry;
 
@@ -17,6 +19,8 @@ import java.util.List;
 import java.util.Optional;
 
 public class OffsetBlockEntity extends LatticeManifestBlockEntity {
+
+    public static int TELEPORT_USAGE = 5;
 
     public OffsetBlockEntity(final BlockPos blockPos, final BlockState blockState) {
         this(SPBlockEntityTypeRegistry.OFFSET.get(), blockPos, blockState);
@@ -30,19 +34,21 @@ public class OffsetBlockEntity extends LatticeManifestBlockEntity {
 
     public static void tick(final @NotNull Level level, final @NotNull BlockPos pos, final @NotNull BlockState state, final @NotNull OffsetBlockEntity blockEntity) {
         LatticeManifestBlockEntity.tick(level, pos, state, blockEntity);
-        final List<Player> players = level.getEntitiesOfClass(Player.class, AABB.ofSize(pos.getCenter(), 1d, 2d, 1d), player -> !player.isSpectator());
-        if (players.isEmpty()) {
+        final List<Entity> entities = level.getEntitiesOfClass(Entity.class, AABB.ofSize(pos.getCenter(), 1d, 2d, 1d), entity -> !entity.isSpectator());
+        if (entities.isEmpty()) {
             return;
         }
-        final Optional<BlockPos> downTarget = nextDownElevator(pos, level);
-        if (downTarget.isEmpty()) {
-            return;
-        }
-        for (final Player player : players) {
-            if (player.isShiftKeyDown()) {
-                if (teleport(player, downTarget.get().above())) {
-                    player.setShiftKeyDown(false);
+        final Optional<BlockPos> upTarget = nextUpElevator(pos, level).map(BlockPos::above);
+        final Optional<BlockPos> downTarget = nextDownElevator(pos, level).map(BlockPos::above);
+        for (final Entity entity : entities) {
+            if (entity.isShiftKeyDown() && downTarget.isPresent()) {
+                if (teleport(entity, downTarget.get(), blockEntity)) {
+                    entity.setShiftKeyDown(false);
                 }
+            }
+            // we move non players always up for now
+            else if (!(entity instanceof Player) && upTarget.isPresent()) {
+                teleport(entity, upTarget.get(), blockEntity);
             }
         }
     }
@@ -50,7 +56,7 @@ public class OffsetBlockEntity extends LatticeManifestBlockEntity {
     private static boolean isValidTarget(final @NotNull BlockPos pos, final @NotNull Level level) {
         final BlockState target = level.getBlockState(pos);
         return level.getBlockState(pos.above()).isAir() && level.getBlockState(pos.above(2)).isAir() &&
-                target.is(SPBlockRegistry.OFFSET);// && target.getValue(LatticeManifestBlock.POWERED);
+                target.is(SPBlockRegistry.OFFSET) && target.getValue(LatticeManifestBlock.POWERED);
     }
 
     public static @NotNull Optional<BlockPos> nextUpElevator(final @NotNull BlockPos pos, final @NotNull Level level) {
@@ -77,11 +83,17 @@ public class OffsetBlockEntity extends LatticeManifestBlockEntity {
         return Optional.empty();
     }
 
-    public static boolean teleport(final @NotNull Player player, final @NotNull BlockPos target) {
-        // TODO: actually use energy
+    public static boolean teleport(final @NotNull Entity entity, final @NotNull BlockPos target, final @NotNull OffsetBlockEntity blockEntity) {
+        final int flame = blockEntity.getCurrentFlame();
+        if (flame < TELEPORT_USAGE) {
+            return false;
+        }
+        if (!(entity instanceof Player player && player.isCreative())) {
+            blockEntity.setCurrentFlame(flame - TELEPORT_USAGE);
+        }
         final Vec3 centeredPos = Vec3.atCenterOf(new Vec3i(target.getX(), target.getY(), target.getZ()));
-        player.teleportTo(centeredPos.x(), target.getY(), centeredPos.z());
-        player.playSound(SoundEvents.ENDERMAN_TELEPORT);
+        entity.teleportTo(centeredPos.x(), target.getY(), centeredPos.z());
+        entity.playSound(SoundEvents.ENDERMAN_TELEPORT);
         return true;
     }
 
