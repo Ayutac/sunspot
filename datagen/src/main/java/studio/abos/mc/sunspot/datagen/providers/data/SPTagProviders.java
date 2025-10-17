a6c6e87844c2af0ab5c1c8866be57820aec6c1e2
Ayutac <fly.high.android@gmail.com>
2025-10-17 18:55:33 +0200

    Made Vex unaffected by Glyph Blocks.

--- datagen/src/main/java/studio/abos/mc/sunspot/datagen/providers/data/SPTagProviders.java
+++ datagen/src/main/java/studio/abos/mc/sunspot/datagen/providers/data/SPTagProviders.java
@@ -3,6 +3,7 @@ package studio.abos.mc.sunspot.datagen.providers.data;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Items;
 import org.jetbrains.annotations.NotNull;
 import studio.abos.mc.sunspot.common.registry.SPEntityTypeRegistry;
@@ -38,10 +39,21 @@ public class SPTagProviders {
 
         @Override
         protected void addTags(final @NotNull HolderLookup.Provider lookup) {
-            getOrCreateTagBuilder(SPTagRegistry.UNAFFECTED_BY_AFFIX).add(SPEntityTypeRegistry.FLAMEFALL.getId());
-            getOrCreateTagBuilder(SPTagRegistry.UNAFFECTED_BY_ASH).add(SPEntityTypeRegistry.FLAMEFALL.getId());
-            getOrCreateTagBuilder(SPTagRegistry.UNAFFECTED_BY_IMPEL).add(SPEntityTypeRegistry.FLAMEFALL.getId());
-            getOrCreateTagBuilder(SPTagRegistry.UNAFFECTED_BY_OFFSET).add(SPEntityTypeRegistry.FLAMEFALL.getId());
+            var unaffectedByAffix = getOrCreateTagBuilder(SPTagRegistry.UNAFFECTED_BY_AFFIX);
+            unaffectedByAffix.add(SPEntityTypeRegistry.FLAMEFALL.getId());
+            unaffectedByAffix.add(EntityType.VEX);
+
+            var unaffectedByAsh = getOrCreateTagBuilder(SPTagRegistry.UNAFFECTED_BY_ASH);
+            unaffectedByAsh.add(SPEntityTypeRegistry.FLAMEFALL.getId());
+            unaffectedByAsh.add(EntityType.VEX);
+
+            var unaffectedByImpel = getOrCreateTagBuilder(SPTagRegistry.UNAFFECTED_BY_IMPEL);
+            unaffectedByImpel.add(SPEntityTypeRegistry.FLAMEFALL.getId());
+            unaffectedByImpel.add(EntityType.VEX);
+
+            var unaffectedByOffset = getOrCreateTagBuilder(SPTagRegistry.UNAFFECTED_BY_OFFSET);
+            unaffectedByOffset.add(SPEntityTypeRegistry.FLAMEFALL.getId());
+            unaffectedByOffset.add(EntityType.VEX);
         }
     }
 }
