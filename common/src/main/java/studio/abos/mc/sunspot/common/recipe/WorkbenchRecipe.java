9bf8e8028880a39057aeec13f4cd2f9afd24121d
Ayutac <fly.high.android@gmail.com>
2025-10-20 13:08:14 +0200

    Added workbench recipe type + datagen builder.

---
+++ common/src/main/java/studio/abos/mc/sunspot/common/recipe/WorkbenchRecipe.java
@@ -0,0 +1,105 @@
+package studio.abos.mc.sunspot.common.recipe;
+
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.util.ExtraCodecs;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.item.crafting.SingleRecipeInput;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
+import studio.abos.mc.sunspot.common.registry.SPRecipeRegistry;
+
+public class WorkbenchRecipe implements Recipe<SingleRecipeInput> {
+
+    protected @NotNull final Ingredient ingredient;
+    protected @NotNull final Ingredient intent;
+    protected final int flame;
+    private @NotNull final ItemStack result;
+
+    public WorkbenchRecipe(final @NotNull Ingredient ingredient, final @NotNull Ingredient intent, final int flame, final @NotNull ItemStack result) {
+        this.ingredient = ingredient;
+        this.intent = intent;
+        if (flame < 0) {
+            throw new IllegalArgumentException("Flame cannot be negative!");
+        }
+        this.flame = flame;
+        this.result = result;
+    }
+
+    @Override
+    public boolean matches(final @NotNull SingleRecipeInput recipeInput, final Level level) {
+        return ingredient.test(recipeInput.item());
+    }
+
+    @Override
+    public @NotNull ItemStack assemble(final SingleRecipeInput recipeInput, final HolderLookup.Provider provider) {
+        return result.copy();
+    }
+
+    @Override
+    public boolean canCraftInDimensions(int i, int j) {
+        return true;
+    }
+
+    @Override
+    public @NotNull ItemStack getResultItem(HolderLookup.Provider provider) {
+        return result;
+    }
+
+    @Override
+    public @NotNull RecipeSerializer<?> getSerializer() {
+        return SPRecipeRegistry.WORKBENCH_SERIALIZER.get();
+    }
+
+    @Override
+    public @NotNull RecipeType<?> getType() {
+        return SPRecipeRegistry.WORKBENCH_TYPE.get();
+    }
+
+    public @NotNull Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public @NotNull Ingredient getIntent() {
+        return intent;
+    }
+
+    public int getFlame() {
+        return flame;
+    }
+
+    public @NotNull ItemStack getResult() {
+        return result;
+    }
+
+    public static class Serializer implements RecipeSerializer<WorkbenchRecipe> {
+        private static final MapCodec<WorkbenchRecipe> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(Ingredient.CODEC.fieldOf("ingredient").forGetter(WorkbenchRecipe::getIngredient), Ingredient.CODEC.fieldOf("intent").forGetter(WorkbenchRecipe::getIntent), ExtraCodecs.NON_NEGATIVE_INT.fieldOf("flame").forGetter(WorkbenchRecipe::getFlame), ItemStack.CODEC.fieldOf("result").forGetter(WorkbenchRecipe::getResult)).apply(instance, WorkbenchRecipe::new));
+        public static final StreamCodec<RegistryFriendlyByteBuf, WorkbenchRecipe> STREAM_CODEC = StreamCodec.of(WorkbenchRecipe.Serializer::toNetwork, WorkbenchRecipe.Serializer::fromNetwork);
+
+        public @NotNull MapCodec<WorkbenchRecipe> codec() {
+            return CODEC;
+        }
+
+        public @NotNull StreamCodec<RegistryFriendlyByteBuf, WorkbenchRecipe> streamCodec() {
+            return STREAM_CODEC;
+        }
+
+        private static WorkbenchRecipe fromNetwork(RegistryFriendlyByteBuf byteBuf) {
+            return new WorkbenchRecipe(Ingredient.CONTENTS_STREAM_CODEC.decode(byteBuf), Ingredient.CONTENTS_STREAM_CODEC.decode(byteBuf), byteBuf.readInt(), ItemStack.STREAM_CODEC.decode(byteBuf));
+        }
+
+        private static void toNetwork(RegistryFriendlyByteBuf byteBuf, WorkbenchRecipe recipe) {
+            Ingredient.CONTENTS_STREAM_CODEC.encode(byteBuf, recipe.getIngredient());
+            Ingredient.CONTENTS_STREAM_CODEC.encode(byteBuf, recipe.getIntent());
+            byteBuf.writeInt(recipe.getFlame());
+            ItemStack.STREAM_CODEC.encode(byteBuf, recipe.getResult());
+        }
+    }
+}
---
+++ datagen/src/main/java/studio/abos/mc/sunspot/datagen/builder/WorkbenchRecipeBuilder.java
@@ -0,0 +1,107 @@
+package studio.abos.mc.sunspot.datagen.builder;
+
+import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.AdvancementRequirements;
+import net.minecraft.advancements.AdvancementRewards;
+import net.minecraft.advancements.Criterion;
+import net.minecraft.advancements.critereon.RecipeUnlockedTrigger;
+import net.minecraft.data.recipes.RecipeBuilder;
+import net.minecraft.data.recipes.RecipeOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.ItemLike;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import studio.abos.mc.sunspot.common.recipe.WorkbenchRecipe;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class WorkbenchRecipeBuilder implements RecipeBuilder {
+
+    private Ingredient ingredient;
+    private Ingredient intent;
+    private int flame;
+    private final ItemStack result;
+    private final Map<String, Criterion<?>> criteria = new LinkedHashMap<>();
+
+    public WorkbenchRecipeBuilder(final @NotNull ItemLike result) {
+        this(new ItemStack(result));
+    }
+
+    public WorkbenchRecipeBuilder(final @NotNull ItemStack result) {
+        this.result = result;
+    }
+
+    @Override
+    public @NotNull WorkbenchRecipeBuilder unlockedBy(final @NotNull String string, final @NotNull Criterion<?> criterion) {
+        criteria.put(string, criterion);
+        return this;
+    }
+
+    @Override
+    public @NotNull WorkbenchRecipeBuilder group(final @Nullable String string) {
+        // group is ignored for now
+        return this;
+    }
+
+    @Override
+    public @NotNull Item getResult() {
+        return result.getItem();
+    }
+
+    public WorkbenchRecipeBuilder requires(final @NotNull ItemLike ingredient) {
+        return requires(Ingredient.of(ingredient));
+    }
+
+    public WorkbenchRecipeBuilder requires(final @NotNull Ingredient ingredient) {
+        if (this.ingredient != null) {
+            throw new IllegalStateException("Ingredient already defined!");
+        }
+        this.ingredient = ingredient;
+        return this;
+    }
+
+    public WorkbenchRecipeBuilder intent(final @NotNull ItemLike ingredient) {
+        return requires(Ingredient.of(ingredient));
+    }
+
+    public WorkbenchRecipeBuilder intent(final @NotNull Ingredient ingredient) {
+        if (this.intent != null) {
+            throw new IllegalStateException("Ingredient already defined!");
+        }
+        this.intent = ingredient;
+        return this;
+    }
+
+    public WorkbenchRecipeBuilder flame(final int flame) {
+        if (flame < 0) {
+            throw new IllegalArgumentException("Flame cannot be negative!");
+        }
+        this.flame = flame;
+        return this;
+    }
+
+    @Override
+    public void save(@NotNull RecipeOutput recipeOutput, @NotNull ResourceLocation resourceLocation) {
+        ensureValid(resourceLocation);
+        final Advancement.Builder builder = recipeOutput.advancement().addCriterion("has_the_recipe", RecipeUnlockedTrigger.unlocked(resourceLocation)).rewards(AdvancementRewards.Builder.recipe(resourceLocation)).requirements(AdvancementRequirements.Strategy.OR);
+        criteria.forEach(builder::addCriterion);
+        final WorkbenchRecipe workbenchRecipe = new WorkbenchRecipe(ingredient, intent, flame, result);
+        recipeOutput.accept(resourceLocation, workbenchRecipe, builder.build(resourceLocation.withPrefix("recipes/sever/")));
+    }
+
+    private void ensureValid(final @NotNull ResourceLocation resourceLocation) {
+        if (criteria.isEmpty()) {
+            throw new IllegalStateException("No way of obtaining recipe " + resourceLocation + "!");
+        }
+        if (ingredient == null) {
+            throw new IllegalStateException("No ingredient specified for recipe " + resourceLocation + "!");
+        }
+        if (intent == null) {
+            throw new IllegalStateException("No intent specified for recipe " + resourceLocation + "!");
+        }
+    }
+}
