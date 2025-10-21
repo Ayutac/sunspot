07e3525cbe4e01178715ab0ed01c37317622bfdc
Ayutac <fly.high.android@gmail.com>
2025-10-21 16:39:25 +0200

    Made the {SEVER} recipe plugin run under Fabric as well.

--- fabric/src/main/java/studio/abos/mc/sunspot/fabric/compat/jei/JeiFabric.java
+++
@@ -1,8 +0,0 @@
-package studio.abos.mc.sunspot.fabric.compat.jei;
-
-import mezz.jei.api.JeiPlugin;
-import studio.abos.mc.sunspot.compat.jei.Jei;
-
-@JeiPlugin
-public class JeiFabric extends Jei {
-}
